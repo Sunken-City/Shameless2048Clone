de015ccfea0ab3d21e6360d5abc61dc76f39391c
PicoRiley <acloudy@mail.smu.edu>
2014-05-01 09:34:40 -0500

    Played around with detecting swipes. Still deciding on how to render everything

---
+++ src/pico/riley/shameless2048clone/ActivitySwipeDetector.java
@@ -0,0 +1,88 @@
+package pico.riley.shameless2048clone;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+//Using code from http://stackoverflow.com/questions/937313/android-basic-gesture-detection
+public class ActivitySwipeDetector implements View.OnTouchListener {
+
+static final String logTag = "ActivitySwipeDetector";
+private Activity activity;
+static final int MIN_DISTANCE = 100;
+private float downX, downY, upX, upY;
+
+public ActivitySwipeDetector(Activity activity){
+    this.activity = activity;
+}
+
+public void onLeftSwipe(){
+    Log.i(logTag, "Left Swipe!");
+    ((MainActivity) activity).onLeftSwipe();
+}
+
+public void onRightSwipe(){
+    Log.i(logTag, "Right Swipe!");
+    ((MainActivity) activity).onRightSwipe();
+}
+
+public void onDownSwipe(){
+    Log.i(logTag, "Down Swipe!");
+    ((MainActivity) activity).onDownSwipe();
+}
+
+public void onUpSwipe(){
+    Log.i(logTag, "Up Swipe!");
+    ((MainActivity) activity).onUpSwipe();
+}
+
+public boolean onTouch(View v, MotionEvent event) {
+    switch(event.getAction()){
+        case MotionEvent.ACTION_DOWN: {
+            downX = event.getX();
+            downY = event.getY();
+            return true;
+        }
+        case MotionEvent.ACTION_UP: {
+            upX = event.getX();
+            upY = event.getY();
+
+            float deltaX = downX - upX;
+            float deltaY = downY - upY;
+
+            // swipe horizontal?
+            if(Math.abs(deltaX) > Math.abs(deltaY))
+            {
+            	if(Math.abs(deltaX) > MIN_DISTANCE){
+                    // left or right
+                    if(deltaX < 0) { this.onRightSwipe(); return true; }
+                    if(deltaX > 0) { this.onLeftSwipe(); return true; }
+                }
+                else {
+                        Log.i(logTag, "Horizontal Swipe was only " + Math.abs(deltaX) + " long, need at least " + MIN_DISTANCE);
+                        return false; // We don't consume the event
+                }
+            }
+            // swipe vertical?
+            else 
+            {
+	            if(Math.abs(deltaY) > MIN_DISTANCE){
+	                // top or down
+	                if(deltaY < 0) { this.onDownSwipe(); return true; }
+	                if(deltaY > 0) { this.onUpSwipe(); return true; }
+	            }
+	            else {
+	                    Log.i(logTag, "Vertical Swipe was only " + Math.abs(deltaX) + " long, need at least " + MIN_DISTANCE);
+	                    return false; // We don't consume the event
+	            }
+            }
+
+            return true;
+        }
+    }
+    return false;
+}
+
+}
--- src/pico/riley/shameless2048clone/MainActivity.java
+++ src/pico/riley/shameless2048clone/MainActivity.java
@@ -1,20 +1,66 @@
 package pico.riley.shameless2048clone;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import android.widget.RelativeLayout.LayoutParams;
 
 //Using this tutorial for the foundation of the project:
 //http://mobile.dzone.com/articles/beginning-android-game
 
 public class MainActivity extends ActionBarActivity {
 
+	static final String logTag = "ActivitySwipeDetector";
+	private Activity activity;
+	static final int MIN_DISTANCE = 100;
+	private float downX, downY, upX, upY;
+    private ImageView imageView;
+    private RelativeLayout lowestLayout;
+    
+    private RelativeLayout.LayoutParams lp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        imageView = (ImageView)findViewById(R.id.bulbasaur);
+        //setContentView(new AndroidTutorialPanel(this));
+        ActivitySwipeDetector activitySwipeDetector = new ActivitySwipeDetector(this);
+        lowestLayout = (RelativeLayout)this.findViewById(R.id.lowestLayout);
+        lowestLayout.setOnTouchListener(activitySwipeDetector);
+    	lp = (LayoutParams) imageView.getLayoutParams();
+    }
+    
+    public void onLeftSwipe()
+    {
+        Toast.makeText(activity, "Left Swipe!", Toast.LENGTH_LONG).show();
+    	//lp.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
     }
 
+	public void onRightSwipe() 
+	{
 
+	    Toast.makeText(activity, "Right Swipe!", Toast.LENGTH_LONG).show();
+    	//lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		
+	}
+	
+	public void onDownSwipe() 
+	{
+
+	    Toast.makeText(activity, "Down Swipe!", Toast.LENGTH_LONG).show();
+    	//lp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+		
+	}
+	
+	public void onUpSwipe() 
+	{
+
+	    Toast.makeText(activity, "Up Swipe!", Toast.LENGTH_LONG).show();
+		//lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		
+	}
 
 }
